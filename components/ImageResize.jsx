d377c451ae7e7754c242d5fb9cd3ad869c758d54
Facu <Facu@MainMacbookPro.local>
2023-02-05 20:09:12 -0300

    props debug

--- components/ImageResize.jsx
+++ components/ImageResize.jsx
@@ -1,59 +1,79 @@
 import Image from "next/image";
+import MidBlock from "./MidBlock";
 
 const horizontalImageSrc = [
   {
-    id: 1,
-    src: "/bannerBlue.webp",
+    src: "/horizontal/first.webp",
+    heading: "Trending now",
+    title: "Lighten up",
+    text: "Refresh your palette with muted tones",
+    button: "Shop",
   },
 ];
 
 const verticalImageSrc = [
   {
-    id: 1,
-    src: "/vertical/verticalOne.webp",
+    src: "/vertical/first.webp",
+    heading: "Trending now",
+    title: "Lighten up",
+    text: "Refresh your palette with muted tones",
+    button: "Shop",
+  },
+  {
+    src: "/vertical/second.webp",
+    heading: "Jordan Women",
+    title: "brooklyn fleece",
+    text: "Refresh your palette with muted tones",
+    button: "Shop",
+    
   },
 ];
 
-const ImageResize = (imageH, imageV) => {
+const ImageResize = (image) => {
   return (
     <>
-      <div>
+      <div className="flex flex-col gap-y-20">
         {horizontalImageSrc.map((image) => (
-          <Image
-            src={image.src}
-            alt={image.imageAlt}
-            width={1200}
-            height={800}
-            className="hidden sm:block"
-          />
+          <div className="hidden sm:block">
+            <h1 className="mb-6 text-2xl font-semibold uppercase ">
+              {image.heading}
+            </h1>
+            <img
+              src={image.src}
+              alt={image.imageAlt}
+              width={1200}
+              height={800}
+            />
+            <div className="flex-col items-center hidden mt-10 sm:block lg:flex">
+              <h1 className="text-5xl font-black uppercase">{image.title}</h1>
+              <p className="mt-4 text-sm text-gray-700">{image.text}</p>
+              <button className="px-6 py-2 mt-8 font-semibold text-white transition duration-300 bg-gray-900 rounded-3xl hover:bg-gray-900/70 ">
+                {image.button}
+              </button>
+            </div>
+          </div>
         ))}
-      </div>
-      <div>
         {verticalImageSrc.map((image) => (
-          <Image
-            src={image.src}
-            alt={image.imageAlt}
-            width={1200}
-            height={800}
-            className="sm:hidden"
-          />
+          <div className="sm:hidden">
+            <h1 className="mb-4 text-2xl font-semibold uppercase ">
+              {image.heading}
+            </h1>
+            <img
+              src={image.src}
+              alt={image.imageAlt}
+              width={1200}
+              height={800}
+            />
+            <div className="flex-col items-center mt-10 lg:flex">
+              <h1 className="text-4xl font-black uppercase">{image.title}</h1>
+              <p className="mt-2 text-sm text-gray-700">{image.text}</p>
+              <button className="px-6 py-2 mt-6 font-semibold text-white transition duration-300 bg-gray-900 rounded-3xl hover:bg-gray-900/70 ">
+                {image.button}
+              </button>
+            </div>
+          </div>
         ))}
       </div>
-
-      {/* 
-        <Image
-          className="hidden sm:block"
-          src={"/bannerblue.webp"}
-          width={1200}
-          height={800}
-        />
-        <Image
-          className="sm:hidden"
-          src={"/vertical/verticalOne.webp"}
-          width={600}
-          height={400}
-        />
-        */}
     </>
   );
 };
--- components/NewHero.jsx
+++ components/NewHero.jsx
@@ -8,9 +8,7 @@ const NewHero = () => {
   return (
     <>
       <div className="flex flex-col justify-center w-screen px-6 mx-auto mt-8 lg:px-12">
-      <h1 className="mb-6 text-2xl font-semibold uppercase ">Trending now</h1>
         <ImageResize />
-        <MidBlock/>
 
       </div>
     </>
