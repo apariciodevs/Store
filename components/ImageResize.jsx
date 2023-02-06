9a21969e61371fd486fe9e580d319d57eaeff420
Facu <Facu@MainMacbookPro.local>
2023-02-06 00:44:16 -0300

    props updated

--- components/ImageResize.jsx
+++ components/ImageResize.jsx
@@ -6,7 +6,31 @@ const horizontalImageSrc = [
     src: "/horizontal/first.webp",
     heading: "Trending now",
     title: "Lighten up",
+    text: "Refresh your palette with muted tones.",
+    button: "Shop",
+  },
+  {
+    id: 2,
+    src: "/horizontal/second.webp",
+    heading: "Jordan women ",
+    title: "brooklyn fleece",
+    text: "Laid-back and elevated.",
+    button: "Shop",
+  },
+  {
+    id: 3,
+    src: "/horizontal/third.webp",
+    heading: "Jordan men",
+    title: "Lighten up",
     text: "Refresh your palette with muted tones",
+    button: "Shop",
+  },
+  {
+    id: 4,
+    src: "/horizontal/fourth.webp",
+    heading: "Jordan kids",
+    title: "jordan 23/7",
+    text: "Explore the latest in kids clothing.",
     button: "Shop",
   },
 ];
@@ -27,7 +51,22 @@ const verticalImageSrc = [
     title: "brooklyn fleece",
     text: "Refresh your palette with muted tones",
     button: "Shop",
-    
+  },
+  {
+    id: 3,
+    src: "/vertical/third.webp",
+    heading: "Jordan Men",
+    title: "Lighten up",
+    text: "Refresh your palette with muted tones",
+    button: "Shop",
+  },
+  {
+    id: 4,
+    src: "/vertical/fourth.webp",
+    heading: "Jordan Kids",
+    title: "Jordan 23/7",
+    text: "Explore the latest in kids clothing.",
+    button: "Shop",
   },
 ];
 
@@ -67,9 +106,9 @@ const ImageResize = (image) => {
               height={800}
             />
             <div className="flex-col items-center mt-10 lg:flex">
-              <h1 className="text-4xl font-black uppercase">{image.title}</h1>
-              <p className="mt-2 text-sm text-gray-700">{image.text}</p>
-              <button className="px-6 py-2 mt-6 font-semibold text-white transition duration-300 bg-gray-900 rounded-3xl hover:bg-gray-900/70 ">
+              <h1 className="text-5xl font-black uppercase">{image.title}</h1>
+              <p className="mt-4 text-sm text-gray-700">{image.text}</p>
+              <button className="px-6 py-2 mt-6 font-semibold text-white transition duration-300 bg-gray-900 rounded-3xl hover:bg-gray-900/80 ">
                 {image.button}
               </button>
             </div>
